3b72646be09356e09ba85210d32429effd91e807
doraemon <dora.godfrey@yale.edu>
2015-04-16 19:36:28 +0000

    First commit of my project

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
-var feeds = require('../feeds.js');
 
 module.exports = router;
 
@@ -38,14 +37,17 @@ mongoose.connect('mongodb://' + process.env.IP + '/networks');
 // =   DEFINE OUR DATA MODELS
 // =
 
-// Define the data structure of a Phrase model
-// It has width, height, top, and left attributes, which are required to be numbers from 0–100
-// And it has a color attribute, which is optionaland is a string (text)
+// Define the data structure of a Submission model
+// It has left, up, right, and down attributes, which should be numbers
+// We'll force them to be integers (with 0 as default) using parseInt in a setter, and also validate their min and max values.
 // Allowed data types (Number, String, Date...): http://mongoosejs.com/docs/schematypes.html
 
-var Phrase = mongoose.model('Phrase', {
-  preposition: {type: String, required: true},
-  noun: {type: String, required: true},
+var Submission = mongoose.model('Submission', {
+  left: {type: Number, min: 0, max: 50, required: true, set: function(v) { return parseInt(v || 0); }},
+  up: {type: Number, min: 0, max: 35, required: true, set: function(v) { return parseInt(v || 0); }},
+  right: {type: Number, min: 0, max: 50, required: true,set: function(v) { return parseInt(v || 0); }},
+  down: {type: Number, min: 0, max: 35, required: true, set: function(v) { return parseInt(v || 0); }},
+  created: {type: Date, default: Date.now}
 });
 
 
@@ -64,10 +66,14 @@ var Phrase = mongoose.model('Phrase', {
 
 router.get('/', function(request, response, toss) {
   
+  // TODO: Arrowheads
+  // TODO: Change direction every night at midnight
+  // TODO: Don't allow values to go off the page
+  
   // When the server receives a request for "/", this code runs
 
   // Find all the Shape records in the database
-  Phrase.find(function(err, phrases) {
+  Submission.find(function(err, submissions) {
     // This code will run once the database find is complete.
     // phrases will contain a list (array) of all the phrases that were found.
     // err will contain errors if any.
@@ -75,34 +81,83 @@ router.get('/', function(request, response, toss) {
     // If there's an error, tell Express to do its default behavior, which is show the error page.
     if (err) return toss(err);
     
-    // The list of shapes will be passed to the template.
-    // Any additional variables can be passed in a similar way (response.locals.foo = bar;)
-    response.locals.phrases = phrases;
+    // Size of a grid unit in pixels
+    var grid_unit_w = 22;
+    var grid_unit_h = 22;
+  
+    // Starting position in grid units
+    var x = 22;
+    var y = 15;
     
-    // Also pass the temperature, wind direction, and next bus arrival.
-    // This can crash if the data hasn't loaded for whatever reason, so toss to Express's error page in that case.
-    try {
+    // Starting color
+    var color = 1;
+    
+    var segments = [];
+    
+    // Go through each submission
+    var i = 0;
+    while (i < submissions.length) {
+      var submission = submissions[i];
       
-      // Use Moment.js to calculate the next bus arrival time minus the current time,
-      // and display it in english e.g. "in 18 minutes".
-      // http://momentjs.com/docs/#/displaying/from/ (returns difference as an english string e.g. "18 minutes")
-      // http://momentjs.com/docs/#/displaying/difference/ (returns difference in milliseconds)
-      var now = moment();
-      var next_arrival = moment(feeds.bus[0].arrival_at);
-      var arriving_in = next_arrival.from(now);
-      var margin = next_arrival.diff(now) / 1000;
+      if (submission.left > 0) {
+        x -= submission.left;
+        segments.push({
+          left: x * grid_unit_h,
+          top: y * grid_unit_h,
+          width: submission.left * grid_unit_w,
+          height: 2,
+          color: color,
+          id: submission.id
+        })
+      }
+      if (submission.up > 0) {
+        y -= submission.up;
+        segments.push({
+          left: x * grid_unit_h,
+          top: y * grid_unit_h,
+          width: 2,
+          height: submission.up * grid_unit_h,
+          color: color,
+          id: submission.id
+        })
+      }
+      if (submission.right > 0) {
+        segments.push({
+          left: x * grid_unit_h,
+          top: y * grid_unit_h,
+          width: submission.right * grid_unit_w,
+          height: 2,
+          color: color,
+          id: submission.id
+        })
+        x += submission.right;
+      }
+      if (submission.down > 0) {
+        segments.push({
+          left: x * grid_unit_h,
+          top: y * grid_unit_h,
+          width: 2,
+          height: submission.down * grid_unit_h,
+          color: color,
+          id: submission.id
+        })
+        y += submission.down;
+      }
 
-      response.locals.temperature = feeds.weather.main.temp;
-      response.locals.font_size = feeds.weather.main.temp / 2;
-      response.locals.wind_direction = feeds.weather.wind.deg;
-      response.locals.arriving_in = arriving_in;
-      response.locals.margin = margin;
-      response.locals.route = feeds.bus[0].route_id;
-      
+      // When we go to the next submission, increase the color
+      color += 1;
+      if (color > 4) {
+        color = 1;
+      }
+
+      i += 1;
     }
-    catch(err) {
-      return toss(err);
-    }
+    
+    // The list of segments will be passed to the template.
+    // Any additional variables can be passed in a similar way (response.locals.foo = bar;)
+    response.locals.segments = segments;
+    response.locals.submissions = submissions.reverse().slice(0, 121);
+    response.locals.remaining = 200 - submissions.length;
     
     // layout tells template to wrap itself in the "layout" template (located in the "views" folder).
     response.locals.layout = 'layout';
@@ -117,52 +172,8 @@ router.get('/', function(request, response, toss) {
 
 
 
-// SHOW PAGE
-// /show?id=54e2058e85b156d10b064ca0
-// Shows a _single_ phrase
-
-router.get('/show', function(request, response, toss) {
-  
-  // When the server receives a request for "/show", this code runs
-  
-  // Find a Phrase with this id
-  Phrase.findOne({_id: request.query.id}, function(err, phrase) {
-    // This code will run once the database find is complete.
-    // phrase will contain the found phrase.
-    // err will contain errors if any (for example, no such record).
-
-    if (err) return toss(err);
-    
-    response.locals.phrase = phrase;
-    response.locals.layout = 'layout';
-    response.render('show');
-    
-  });
-  
-});
-
-
-
-// NEW PAGE
-// /new
-
-router.get('/new', function(request, response) {
-
-  // When the server receives a request for "/new", this code runs
-  
-  // Just render a basic HTML page with a form. We don't need to pass any variables.
-
-  response.locals.layout = 'layout';
-  response.render('new');
-  
-  // Please see views/new.hbs for additional comments
-  
-});
-
-
-
 // CREATE PAGE
-// /create?width=25&height=25&top=25&left=25&color=#ff0000
+// /create?up=1&down=2&right=3&left=4
 // Normally you get to this page by clicking "Submit" on the /new page, but
 // you could also enter a URL like the above directly into your browser.
 
@@ -171,44 +182,53 @@ router.get('/create', function(request, response, toss) {
   // When the server receives a request for "/create", this code runs
   
   response.locals.layout = 'layout';
-
-  // Make a new Phrase in memory, with the parameters that come from the URL 
-  // ?width=25&height=25&top=25&left=25&color=#ff0000
-  // and store it in the shape variable
-  var phrase = new Phrase({
-    preposition: request.query.preposition,
-    noun: request.query.noun,
-  });
   
-  // Now save it to the database
-  phrase.save(function(err) {
-    // This code runs once the database save is complete
-
-    // An err here can be due to validations
+  // Count the number of submissions so far
+  Submission.count(function(err, how_many) {
     if (err) return toss(err);
+    if (how_many >= 200) {
+      console.log("Resetting!");
+      Submission.find().remove(function(err) {
+        if (err) return toss(err);
+      });
+    }
     
-    // Otherwise render a "thank you" page
-    response.locals.phrase = phrase;
-    response.render('create');
+    // Make a new Submission in memory, with the parameters that come from the URL 
+    // and store it in the submission variable
+    // TODO: When a remove happens, this hsould really happen in the remove callback, otherwise \
+    //   the remove could happen after the new segment is added.
+    var submission = new Submission({
+      up: request.query.up,
+      down: request.query.down,
+      left: request.query.left,
+      right: request.query.right,
+    });
     
-    // Alternatively we could just do
-    // response.redirect('/');
-    // to send the user straight to the homepage after saving the new shape
-
+    // Now save it to the database
+    submission.save(function(err) {
+      // This code runs once the database save is complete
+  
+      // An err here can be due to validations
+      if (err) return toss(err);
+      
+      // Instead of rendering a "thank you" page, just redirect home now
+      response.redirect('/');
+  
+    });  
+    
   });
   
 });
 
 
 
-// ABOUT PAGE
-// /about
+// RESET PAGE
+// /reset
+// DANGER! Clears the whole database with no warning
 
-router.get('/about', function(request, response) {
-
-  // When the server receives a request for "/about", this code runs
-
-  response.locals.layout = 'layout';
-  response.render('about');
-  
+router.get('/reset', function(request, response, toss) {
+  Submission.find().remove(function(err) {
+    if (err) return toss(err);
+    response.redirect('/');
+  });
 });
